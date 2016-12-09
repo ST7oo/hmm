b8f51eada7c001805232daa55d60f80da432b246
ST7oo <reln13st@gmail.com>
2016-12-09 18:52:49 +0100

    baum welch

--- HMM.py
+++ HMM.py
@@ -74,16 +74,107 @@ class HMM:
         return sequences
 
     def forward(self, observations):
-        sequences = []
-        for i in np.arange(len(observations)):
-            T = len(observations[i])
-            c = np.zeros([T])
-            alpha = np.zeros([self.N, T])
-            index_obs = self.name_observations.index(observations[i][0])
-            alpha[:, 0] = self.A[0, :] * self.B[:, index_obs]
-        return sequences
+        # sequences = []
+        # for i in np.arange(len(observations)):
+        T = len(observations)
+        c = np.zeros([T])
+        alpha = np.zeros([self.N, T])
+        index_obs = self.name_observations.index(observations[0])
+        alpha[:, 0] = self.A[0, :] * self.B[:, index_obs]
+        c[0] = 1.0 / np.sum(alpha[:, 0])
+        alpha[:, 0] *= c[0]
+        for t in np.arange(1, T):
+            index_obs = self.name_observations.index(observations[t])
+            alpha[:, t] = np.dot(
+                alpha[:, t - 1], self.A) * self.B[:, index_obs]
+            c[t] = 1.0 / np.sum(alpha[:, t])
+            alpha[:, t] *= c[t]
+        log_prob_obs = -(np.sum(np.log(c)))
+        return alpha, log_prob_obs, c
+        # sequences.append((alpha, log_prob_obs, c))
+        # return sequences
 
-    # def baum_welch(self, A, B, observations):
+    def backward(self, observations, c):
+        # sequences = []
+        # for i in np.arange(len(observations)):
+        T = len(observations)
+        beta = np.zeros([self.N, T])
+        beta[:, T - 1] = c[T - 1]
+        for t in np.arange(T - 1, 0, -1):
+            index_obs = self.name_observations.index(observations[t])
+            beta[:, t - 1] = np.dot(self.A,
+                                    self.B[:, index_obs] * beta[:, t])
+            beta[:, t - 1] *= c[t - 1]
+        return beta
+        #     sequences.append(beta)
+        # return sequences
+
+    def bw(self, observations, max_iter=20):
+        log_likelihoods = []
+        for epoch in np.arange(max_iter):
+            for obs in observations:
+                alpha, log_prob_obs, c = self.forward(obs)
+                beta = self.backward(obs, c)
+                T = len(obs)
+                for t in np.arange(T - 1):
+                    index_obs = self.name_observations.index(obs[t + 1])
+
+    def baum_welch(self, observations, max_iter=20):
+        # best_A = self.A.copy()
+        # best_B = self.B.copy()
+        log_likelihoods = []
+        # val_log_likelihoods = []
+        for epoch in np.arange(max_iter):
+            log_likelihood = 0
+            b_bar_den = np.zeros([self.N])
+            a_bar_den = np.zeros([self.N])
+            # si_sj_all = np.zeros([self.N, self.N])
+            a_bar_num = np.zeros([self.N, self.N])
+            pi_bar = np.zeros([self.N])
+            b_bar_num = np.zeros([self.N, self.M])
+            for obs in observations:
+                alpha, log_prob_obs, c = self.forward(obs)
+                beta = self.backward(obs, c)
+                log_likelihood += log_prob_obs
+                T = len(obs)
+                w_k = 1.0 / -(log_prob_obs + np.log(T))
+                gamma_raw = alpha * beta
+                gamma = gamma_raw / gamma_raw.sum(0)
+                pi_bar += w_k * gamma[:, 0]
+                b_bar_den += w_k * gamma.sum(1)
+                a_bar_den += w_k * gamma[:, :T - 1].sum(1)
+                xi = np.zeros([self.N, self.N, T - 1])
+                for t in np.arange(T - 1):
+                    index_obs = self.name_observations.index(obs[t + 1])
+                    for i in np.arange(self.N):
+                        xi[i, :, t] = alpha[i, t] * self.A[i, :] * \
+                            self.B[:, index_obs] * beta[:, t + 1]
+                # si_sj_all += w_k * xi.sum(2)
+                a_bar_num += w_k * xi[:, :, :T - 1].sum(2)
+                B_bar = np.zeros([self.N, self.M])
+                for k in np.arange(self.M):
+                    indicator = np.array(
+                        [self.name_observations[k] == x for x in obs])
+                    B_bar[:, k] = gamma.T[indicator, :].sum(0)
+                b_bar_num += w_k * B_bar
+            # update A
+            A_bar = np.zeros([self.N, self.N])
+            A_bar[0, :] = pi_bar / np.sum(pi_bar)
+            for i in np.arange(1, self.N - 1):
+                A_bar[i, :] = a_bar_num[i, :] / a_bar_den[i]
+            self.A = A_bar
+            # update B
+            for i in np.arange(self.N):
+                if b_bar_den[i] > 0:
+                    b_bar_num[i, :] = b_bar_num[i, :] / b_bar_den[i]
+                else:
+                    b_bar_num[i, :] = b_bar_num[i, :]
+            self.B = b_bar_num
+            log_likelihoods.append(log_likelihood)
+            if epoch > 1 and log_likelihoods[epoch - 1] == log_likelihood:
+                print('plateaued')
+                break
+        return self
 
 
 states = ['INIT', 'Onset', 'Mid', 'End', 'FINAL']
@@ -107,11 +198,168 @@ B1 = np.array([[0, 0, 0, 0, 0, 0, 0],
                [0, 0, 0.2, 0.7, 0.1, 0, 0],
                [0, 0, 0, 0.1, 0, 0.5, 0.4],
                [0, 0, 0, 0, 0, 0, 0]])
+A_ini = np.array([[0, 1, 0, 0, 0],
+                  [0, 0.5, 0.5, 0, 0],
+                  [0, 0, 0.5, 0.5, 0],
+                  [0, 0, 0, 0.5, 0.5],
+                  [0, 0, 0, 0, 0]])
+B_ini = np.array([[0, 0, 0, 0, 0, 0, 0],
+                  [0.33, 0.33, 0.33, 0, 0, 0, 0],
+                  [0, 0, 0.33, 0.33, 0.33, 0, 0],
+                  [0, 0, 0, 0.33, 0, 0.33, 0.33],
+                  [0, 0, 0, 0, 0, 0, 0]])
 seq = [['C1', 'C2', 'C3', 'C4', 'C4', 'C6', 'C7'],
-       ['C2', 'C2', 'C5', 'C4', 'C4', 'C6', 'C6'],
-       ['C1', 'C2', 'C3', 'C6']]
-h = HMM(A1, B1, states, observations)
+       ['C2', 'C2', 'C5', 'C4', 'C4', 'C6', 'C6']]
+seq1 = [['C1', 'C2', 'C3', 'C4', 'C4', 'C6', 'C7'],
+        ['C2', 'C2', 'C5', 'C4', 'C4', 'C6', 'C6'],
+        ['C1', 'C2', 'C3', 'C6']]
+seq_train = [['C1', 'C4', 'C6', 'C7', 'C6'],
+             ['C1', 'C5', 'C7'],
+             ['C2', 'C1', 'C3', 'C1', 'C1', 'C4', 'C4', 'C4', 'C4', 'C5', 'C4', 'C4', 'C4',
+                 'C4', 'C5', 'C5', 'C3', 'C4', 'C4', 'C3', 'C3', 'C5', 'C7', 'C6', 'C7'],
+             ['C2', 'C1', 'C5', 'C4', 'C4', 'C4', 'C4',
+              'C4', 'C4', 'C4', 'C5', 'C4', 'C4', 'C6'],
+             ['C3', 'C4', 'C7', 'C7'],
+             ['C2', 'C3', 'C4', 'C3', 'C4', 'C3', 'C6', 'C6'],
+             ['C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C5', 'C4', 'C4',
+              'C4', 'C5', 'C4', 'C3', 'C3', 'C4', 'C4', 'C4', 'C3', 'C6'],
+             ['C3', 'C3', 'C1', 'C4', 'C3', 'C5',
+                 'C4', 'C4', 'C4', 'C4', 'C4', 'C6'],
+             ['C2', 'C3', 'C5', 'C4', 'C7'],
+             ['C2', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C6'],
+             ['C1', 'C4', 'C6', 'C7'],
+             ['C1', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C5', 'C4',
+              'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C5', 'C4', 'C3', 'C5', 'C7'],
+             ['C2', 'C4', 'C7'],
+             ['C3', 'C4', 'C7'],
+             ['C2', 'C1', 'C4', 'C4', 'C4', 'C4', 'C3',
+              'C4', 'C4', 'C4', 'C3', 'C4', 'C5', 'C7'],
+             ['C1', 'C4', 'C4', 'C3', 'C3', 'C4', 'C4', 'C4', 'C6', 'C7'],
+             ['C1', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C6'],
+             ['C1', 'C3', 'C4', 'C4', 'C3', 'C5', 'C4', 'C4', 'C3', 'C4', 'C4', 'C3', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3',
+              'C5', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C7'],
+             ['C1', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4',
+              'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C6', 'C7'],
+             ['C3', 'C4', 'C6'],
+             ['C2', 'C4', 'C4', 'C5', 'C6'],
+             ['C2', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4',
+              'C3', 'C4', 'C4', 'C4', 'C3', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C7'],
+             ['C1', 'C5', 'C4', 'C4', 'C7'],
+             ['C1', 'C4', 'C5', 'C4', 'C6'],
+             ['C3', 'C4', 'C5', 'C4', 'C4', 'C3', 'C4', 'C5', 'C4', 'C3', 'C3', 'C4', 'C4', 'C4',
+              'C4', 'C4', 'C5', 'C4', 'C5', 'C4', 'C4', 'C5', 'C4', 'C4', 'C4', 'C4', 'C6'],
+             ['C3', 'C3', 'C3', 'C4', 'C3', 'C5', 'C3', 'C4',
+              'C5', 'C4', 'C4', 'C4', 'C4', 'C6', 'C4'],
+             ['C3', 'C5', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C6'],
+             ['C3', 'C2', 'C1', 'C1', 'C2', 'C3', 'C3', 'C6', 'C6', 'C6'],
+             ['C3', 'C4', 'C4', 'C4', 'C3', 'C6', 'C6'],
+             ['C2', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C5',
+              'C4', 'C3', 'C3', 'C4', 'C4', 'C4', 'C3', 'C5', 'C3', 'C4', 'C3', 'C7', 'C6'],
+             ['C1', 'C4', 'C5', 'C4', 'C6', 'C6'],
+             ['C3', 'C1', 'C4', 'C5', 'C4', 'C4', 'C4', 'C4',
+              'C3', 'C3', 'C3', 'C4', 'C4', 'C4', 'C7', 'C4'],
+             ['C1', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C7', 'C4'],
+             ['C3', 'C1', 'C4', 'C4', 'C4', 'C5', 'C4', 'C4', 'C4', 'C4', 'C7'],
+             ['C2', 'C4', 'C4', 'C3', 'C4', 'C3', 'C4', 'C4', 'C4', 'C3', 'C3', 'C4', 'C4', 'C5', 'C4', 'C5', 'C4', 'C4',
+              'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C3', 'C3', 'C4', 'C4', 'C3', 'C4', 'C3', 'C4', 'C4', 'C4', 'C3', 'C6'],
+             ['C1', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C6'],
+             ['C2', 'C1', 'C4', 'C4', 'C5', 'C4', 'C4', 'C4', 'C3', 'C3', 'C4', 'C4', 'C4', 'C4', 'C3', 'C3', 'C4', 'C4', 'C4',
+              'C4', 'C3', 'C4', 'C5', 'C4', 'C4', 'C4', 'C4', 'C3', 'C3', 'C5', 'C4', 'C4', 'C4', 'C4', 'C5', 'C4', 'C4'],
+             ['C1', 'C3', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C3', 'C6', 'C6'],
+             ['C1', 'C4', 'C5', 'C4'],
+             ['C2', 'C1', 'C2', 'C3', 'C3', 'C4', 'C4', 'C5', 'C4', 'C3', 'C6'],
+             ['C1', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C5', 'C4', 'C4',
+              'C4', 'C3', 'C4', 'C4', 'C5', 'C5', 'C3', 'C4', 'C7', 'C7'],
+             ['C2', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C3', 'C4', 'C4',
+              'C4', 'C4', 'C4', 'C4', 'C3', 'C5', 'C4', 'C4', 'C4', 'C4', 'C4', 'C5', 'C4', 'C7', 'C6', 'C7'],
+             ['C2', 'C3', 'C6'],
+             ['C3', 'C5', 'C4', 'C4', 'C4', 'C4', 'C4',
+                 'C3', 'C4', 'C6', 'C6', 'C4', 'C7'],
+             ['C2', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C3', 'C4', 'C7'],
+             ['C3', 'C1', 'C1', 'C1', 'C2', 'C4', 'C4', 'C4',
+              'C3', 'C4', 'C4', 'C6', 'C6', 'C6', 'C4', 'C7'],
+             ['C3', 'C2', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C3', 'C5', 'C4', 'C5', 'C3', 'C4',
+              'C4', 'C4', 'C5', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C3', 'C3', 'C6', 'C4'],
+             ['C1', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C5', 'C4', 'C4', 'C5', 'C4', 'C4',
+              'C3', 'C4', 'C3', 'C4', 'C3', 'C4', 'C3', 'C4', 'C3', 'C4', 'C7', 'C7', 'C7'],
+             ['C1', 'C4', 'C4', 'C5', 'C4', 'C4', 'C4',
+                 'C4', 'C4', 'C5', 'C6', 'C6', 'C6'],
+             ['C2', 'C4', 'C4', 'C4', 'C6', 'C7'],
+             ['C3', 'C4', 'C4', 'C3', 'C4', 'C3', 'C4', 'C4', 'C4',
+              'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C7', 'C7', 'C6'],
+             ['C3', 'C4', 'C4', 'C3', 'C4', 'C4', 'C5', 'C4', 'C4', 'C4', 'C7'],
+             ['C3', 'C4', 'C4', 'C6', 'C7'],
+             ['C1', 'C4', 'C4', 'C4', 'C7', 'C6'],
+             ['C3', 'C1', 'C3', 'C3', 'C4', 'C4', 'C4', 'C6'],
+             ['C3', 'C1', 'C4', 'C3', 'C4', 'C4',
+                 'C4', 'C4', 'C4', 'C3', 'C4', 'C6'],
+             ['C3', 'C1', 'C1', 'C1', 'C1', 'C3', 'C7', 'C6'],
+             ['C1', 'C4', 'C6', 'C6'],
+             ['C3', 'C4', 'C3', 'C4', 'C4', 'C4', 'C3', 'C3', 'C3', 'C6'],
+             ['C1', 'C3', 'C5', 'C5', 'C4', 'C3', 'C4', 'C4', 'C4', 'C7'],
+             ['C3', 'C3', 'C7', 'C7'],
+             ['C1', 'C3', 'C3', 'C4', 'C3', 'C5',
+                 'C5', 'C3', 'C4', 'C3', 'C4', 'C6'],
+             ['C1', 'C3', 'C6', 'C6'],
+             ['C1', 'C4', 'C5', 'C4', 'C4', 'C6', 'C6'],
+             ['C2', 'C1', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C3', 'C4'],
+             ['C3', 'C5', 'C4', 'C3', 'C3', 'C4', 'C4',
+                 'C4', 'C3', 'C4', 'C5', 'C4', 'C7'],
+             ['C3', 'C4', 'C4', 'C4', 'C5', 'C4', 'C4', 'C4', 'C4', 'C4',
+              'C4', 'C4', 'C3', 'C4', 'C4', 'C3', 'C4', 'C7', 'C7'],
+             ['C2', 'C1', 'C3', 'C3', 'C4', 'C3', 'C4',
+                 'C3', 'C4', 'C4', 'C4', 'C4', 'C6'],
+             ['C2', 'C1', 'C4', 'C5', 'C4', 'C4', 'C7', 'C4', 'C6', 'C6'],
+             ['C1', 'C1', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C7'],
+             ['C3', 'C1', 'C4', 'C5', 'C4', 'C6'],
+             ['C1', 'C3', 'C4', 'C3', 'C5', 'C4', 'C3', 'C4',
+              'C4', 'C4', 'C4', 'C3', 'C6', 'C6', 'C6'],
+             ['C2', 'C1', 'C3', 'C1', 'C1', 'C3', 'C4', 'C4', 'C4', 'C6', 'C6'],
+             ['C1', 'C2', 'C3', 'C5', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3',
+              'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C7', 'C6'],
+             ['C3', 'C3', 'C4', 'C4', 'C6'],
+             ['C1', 'C4', 'C4', 'C4', 'C5', 'C3', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C3', 'C4', 'C3', 'C4', 'C4', 'C3', 'C4', 'C3', 'C4',
+              'C3', 'C4', 'C4', 'C4', 'C4', 'C5', 'C3', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4', 'C3', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C6'],
+             ['C1', 'C3', 'C4', 'C4', 'C6', 'C6', 'C7'],
+             ['C2', 'C3', 'C4', 'C4', 'C4', 'C4', 'C7'],
+             ['C1', 'C3', 'C4', 'C4', 'C6', 'C6'],
+             ['C1', 'C3', 'C3', 'C4', 'C3', 'C3', 'C4', 'C4', 'C4', 'C4', 'C5', 'C4',
+              'C4', 'C5', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C3', 'C3', 'C7'],
+             ['C2', 'C1', 'C4', 'C3', 'C3', 'C4', 'C3', 'C4', 'C4', 'C4',
+              'C3', 'C4', 'C4', 'C3', 'C4', 'C5', 'C4', 'C5', 'C6', 'C7'],
+             ['C1', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4',
+              'C4', 'C4', 'C3', 'C3', 'C5', 'C5', 'C5', 'C4', 'C3', 'C4', 'C4', 'C4', 'C3', 'C7'],
+             ['C1', 'C4', 'C4', 'C4', 'C5', 'C4', 'C4',
+              'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C7'],
+             ['C2', 'C4', 'C5', 'C4', 'C4', 'C4', 'C4', 'C4', 'C3', 'C4',
+              'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C7', 'C6'],
+             ['C2', 'C4', 'C3', 'C5', 'C4', 'C3', 'C4', 'C4', 'C4', 'C3', 'C4', 'C4',
+              'C4', 'C4', 'C4', 'C4', 'C4', 'C5', 'C5', 'C4', 'C3', 'C3', 'C3', 'C6'],
+             ['C1', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4',
+              'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C6', 'C6'],
+             ['C3', 'C3', 'C4', 'C4', 'C4', 'C4',
+                 'C5', 'C4', 'C4', 'C4', 'C5', 'C7'],
+             ['C3', 'C4', 'C4'],
+             ['C1', 'C4', 'C4', 'C7'],
+             ['C1', 'C3', 'C4', 'C5', 'C3', 'C4', 'C7', 'C6'],
+             ['C3', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C7', 'C4', 'C7'],
+             ['C1', 'C3', 'C4', 'C4'],
+             ['C3', 'C3', 'C4', 'C4', 'C4', 'C4', 'C6'],
+             ['C3', 'C3', 'C5', 'C4', 'C5', 'C4'],
+             ['C1', 'C1', 'C4', 'C4', 'C3', 'C4', 'C4', 'C4', 'C4', 'C4',
+              'C4', 'C4', 'C3', 'C4', 'C6', 'C7', 'C6', 'C6', 'C7'],
+             ['C3', 'C3', 'C3', 'C4', 'C4', 'C7', 'C6'],
+             ['C3', 'C1', 'C3', 'C4', 'C4', 'C3', 'C3', 'C6'],
+             ['C2', 'C3', 'C3', 'C4', 'C4', 'C5', 'C6', 'C7'],
+             ['C2', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4', 'C4',
+              'C4', 'C4', 'C4', 'C3', 'C4', 'C4', 'C3', 'C7'],
+             ['C1', 'C3', 'C2', 'C4', 'C4', 'C6', 'C7', 'C7']]
+# h = HMM(A1, B1, states, observations)
 # h = HMM(A, B, states, observations)
 # print(h.gen_sequence(3))
 # print(h.viterbi(seq))
-print(h.forward(seq))
+# print(h.forward(seq))
+h1 = HMM(A_ini, B_ini, states, observations)
+h2 = h1.baum_welch(seq_train)
+print(h2.A)
+print(h2.B)
